8a7d3c6f9a1eaa843be50c989c28f61f3e934514
Nikita <nikita.kamyshenko@mail.ru>
2018-01-11 04:05:31 +0300

    Fix hide label

--- common.blocks/workspace/js/workspace__v-text.js
+++ common.blocks/workspace/js/workspace__v-text.js
@@ -1,7 +1,23 @@
 function VText(text) {
 	let $element;
 	let vObject;
+	let autoHideLabel = true;
 	init();
+
+	function createLabel() {
+		let $label = $('<div style="width: 100%; height: 15px; background-color: #0b58a2"></div>');
+		if (autoHideLabel) {
+			$label.css('background-color', 'rgba(0, 125, 215, 0)');
+			vObject.getElement().mousemove(function () {
+				$label.css('background-color', '#0b58a2');
+
+			});
+			vObject.getElement().mouseout(function () {
+				$label.css('background-color', 'rgba(0, 125, 215, 0)');
+			});
+		}
+		return $label;
+	}
 
 	function init() {
 		$element = $(`<div class="workspace__v-text" contenteditable="true">${text}</div>`);
@@ -14,7 +30,8 @@ function VText(text) {
 		$element.blur(function () {
 			vObject.getElement().draggable({disabled: false});
 		});
-		let $label = $('<div style="width: 100%; height: 15px; background-color: #0b58a2"></div>');
+		let $label = createLabel();
+
 		vObject.addContent($label);
 		vObject.addContent($element);
 	}
